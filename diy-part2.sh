dfd0980ca80aeab5b5d9e823f7ac5e147211af49
Faspand <zhaokuanhong@live.cn>
2023-09-11 10:20:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,30 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 使用sed命令添加自定义防火墙规则到firewall.user文件中
+sed -i '/# Custom rules/a \
+# 通过 rkp-ipid 设置 IPID
+# 若没有加入 rkp-ipid 模块，此部分不需要加入
+iptables -t mangle -N IPID_MOD
+iptables -t mangle -A FORWARD -j IPID_MOD
+iptables -t mangle -A OUTPUT -j IPID_MOD
+iptables -t mangle -A IPID_MOD -d 0.0.0.0/8 -j RETURN
+iptables -t mangle -A IPID_MOD -d 127.0.0.0/8 -j RETURN
+# 由于本校局域网是 A 类网，所以我将这一条注释掉了，具体要不要注释结合你所在的校园网内网类型
+# iptables -t mangle -A IPID_MOD -d 10.0.0.0/8 -j RETURN
+iptables -t mangle -A IPID_MOD -d 172.16.0.0/12 -j RETURN
+iptables -t mangle -A IPID_MOD -d 192.168.0.0/16 -j RETURN
+iptables -t mangle -A IPID_MOD -d 255.0.0.0/8 -j RETURN
+iptables -t mangle -A IPID_MOD -j MARK --set-xmark 0x10/0x10
+# 防时钟偏移检测
+iptables -t nat -N ntp_force_local
+iptables -t nat -I PREROUTING -p udp --dport 123 -j ntp_force_local
+iptables -t nat -A ntp_force_local -d 0.0.0.0/8 -j RETURN
+iptables -t nat -A ntp_force_local -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A ntp_force_local -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A ntp_force_local -s 192.168.0.0/16 -j DNAT --to-destination 192.168.1.1
+# 通过 iptables 修改 TTL 值
+iptables -t mangle -A POSTROUTING -j TTL --ttl-set 64
+' package/network/config/firewall/files/firewall.user
+sed -i 's/CONFIG_BUILDSUFFIX=\".*\"/CONFIG_BUILDSUFFIX=\"-Faspand\"/' include/image.mk
+
